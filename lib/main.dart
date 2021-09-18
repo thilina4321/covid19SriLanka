5cef5292d3be2c9a67a5e8f79b37ffde43896e1b
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-09-18 21:41:21 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
       routes: {
         HomePage.routeName: (ctx) => HomePage(),
       },
