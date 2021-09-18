a2aae75745b992852dd392ccccd16bf626d62ba7
Ishara Dilshan <hridmaduranga@gmail.com>
2021-09-18 19:58:24 +0530

    util functions added

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,7 +1,9 @@
+import 'package:covidsrilanka/utils/util_functions.dart';
+import 'package:covidsrilanka/welcome_screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -9,9 +11,25 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 5), () {
+      UtilFunctions.navigateTo(WelcomeScreen(), context);
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: Image.asset("assets/images/virus.png")
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
